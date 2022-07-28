03eaa64dc23906986c0674224193cd6843408e94
Sermakani-V <110027186+Sermakani-V@users.noreply.github.com>
2022-07-28 22:25:50 +0530

    Add files via upload

---
+++ program/Base1.java
@@ -0,0 +1,15 @@
+
+class Base1
+{
+   public static void main(String[]ref)
+   {
+      int value= 56;
+	  char value1= 'S';
+	  
+	  System.out.println(value);
+	  System.out.println(value1);
+	}
+
+}	
+	  
+	  
