cffd98c0c9c08a4d748ee3aa59f7c87e6911cdee
Sermakani-V <110027186+Sermakani-V@users.noreply.github.com>
2022-07-29 13:57:30 +0530

    Add files via upload

---
+++ program/Program3.java
@@ -0,0 +1,27 @@
+
+/*
+@Author: Sermakani V
+Date:28/07/2022
+Title: Re-initializing variables
+*/
+
+class Program3
+
+{
+
+   public static void main(String[]ref)
+   
+   {
+	    int val= 10;
+		System.out.println(val);
+		
+		val= 20;         //re-initializing
+		
+		System.out.println(val);
+		System.out.println(val);
+		System.out.println("End Program");
+		
+		
+    }
+	
+}
