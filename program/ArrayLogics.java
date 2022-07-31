ade346abca305ad2d534a0249ff88c3eec4643d0
Sermakani-V <110027186+Sermakani-V@users.noreply.github.com>
2022-07-31 14:50:15 +0530

    Add files via upload

---
+++ program/ArrayLogics.java
@@ -0,0 +1,13 @@
+class ArrayLogics
+{
+
+  public static void main(String[]ref)
+   {
+     int arr[] = {10,20,30,40,50,60,70,80,90,100};
+	 
+	  System.out.println(arr[3]);
+	  System.out.println(arr.length);
+	  System.out.println("7th index value:"+ arr[3]);
+	}
+	
+}
