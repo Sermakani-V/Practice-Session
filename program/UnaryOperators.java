6dadf7968f88443956e553d7568f3d624e77e6fe
Sermakani-V <110027186+Sermakani-V@users.noreply.github.com>
2022-07-31 11:55:17 +0530

    Add files via upload

---
+++ program/UnaryOperators.java
@@ -0,0 +1,19 @@
+class UnaryOperators
+{
+	
+	public static void main(String[]ref)
+	{		
+		int value =10;
+		int value1 = 20;
+			System.out.println(value); 
+			System.out.println(++value);   
+			System.out.println(++value);   
+			System.out.println(++value);
+			System.out.println(value);   
+			System.out.println(--value);   
+			System.out.println(value);     
+			System.out.println(--value);   
+			System.out.println(value);     
+				
+	}
+}
