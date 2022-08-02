1c228f13c8cc4cc8fad456ef3c0340d6aab1e6cb
Sermakani-V <110027186+Sermakani-V@users.noreply.github.com>
2022-08-02 17:35:32 +0530

    Add files via upload

---
+++ program/AssignmentOperator.java
@@ -0,0 +1,14 @@
+class AssignmentOperator
+{
+  public static void main(String[]sname)
+  {
+     int X=10;
+	 
+	 System.out.println("Value of X is " + X);
+	 
+	 X+=20;
+	 
+	 System.out.println("Value of X is " + X);
+   }
+   
+}
